7c66d4f8c0910fa22edc044960a91cce5f007f5d
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-11-27 12:46:00 +0500

    create a new task and save it to database

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Task from './Task';
 import TaskForm from './TaskForm';
+import { toast } from 'react-toastify';
 
 export default function TaskList() {
+  const [formData, setFormData] = useState({
+    name: '',
+    completed: false,
+  });
+
+  const { name } = formData;
+
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const createTask = async (evt) => {
+    evt.preventDefault();
+    if (name === '') {
+      return toast.error('Input field cannot be empty');
+    }
+    try {
+      await axios.post('/api/tasks', formData);
+      setFormData({ ...formData, name: '' });
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div>
       <h2>Task Manager</h2>
-      <TaskForm />
+      <TaskForm
+        createTask={createTask}
+        handleInputChange={handleInputChange}
+        name={name}
+      />
       <div className="--flex-between --pb">
         <p>
           <b>Total Tasks:</b> 0
